25713b10ed29fc9fb0f827512a9ddab14a8b2354
Gannesh <ganeshbreed@gmail.com>
2022-09-07 13:53:47 +0200

    Added Measure, Column and report page

--- Retail Analysis/Model/queries/Managers.m
+++ Retail Analysis/Model/queries/Managers.m
@@ -1,6 +1,7 @@
 let
     Source = Excel.Workbook(File.Contents("C:\Users\Ganesh.Breed\OneDrive - MOYO Business Advisory\Desktop\Moyo Projects\Power BI - Best Practice\retail_analysis_webinar.xlsx"), null, true),
     Managers_Table = Source{[Item="Managers",Kind="Table"]}[Data],
-    #"Changed Type" = Table.TransformColumnTypes(Managers_Table,{{"Suburb", type text}, {"Postcode", Int64.Type}, {"Manager", type text}})
+    #"Changed Type" = Table.TransformColumnTypes(Managers_Table,{{"Suburb", type text}, {"Postcode", Int64.Type}, {"Manager", type text}}),
+    #"Renamed Columns" = Table.RenameColumns(#"Changed Type",{{"Manager", "Manager Name"}})
 in
-    #"Changed Type"
+    #"Renamed Columns"
